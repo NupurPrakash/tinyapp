bd957ccf235b49a4b09591552c481b2dc6a09be1
NupurPrakash <prakashnupurprakash@gmail.com>
2020-07-06 17:38:00 +0000

    Added a http response string

--- express_server.js
+++ express_server.js
@@ -16,6 +16,9 @@ app.get("/urls.json", (req, res) => {
 
 });
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n")
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT} !`);
