78635e1836a4ecb1da3bc365e4d02744a0976c3d
NupurPrakash <prakashnupurprakash@gmail.com>
2020-07-07 10:07:29 +0000

    submitted random url and getting the shortlink for that page

--- express_server.js
+++ express_server.js
@@ -51,21 +51,31 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
-});
-
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
   let longURL = req.body.longURL;
-  urlDatabase[shortURL] = longURL;
+  urlDatabase[shortURL] = longURL
   console.log(req.body);
   console.log(longURL);
   console.log(shortURL);
   console.log(urlDatabase);
-  res.send("Responding with a redirect");
+  //res.send("Responding with a redirect");
+  res.redirect(`/urls/${shortURL}`);
 });
+
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+/*
+app.post("urls/:shortURL", (req, res) => {
+  urlDatabase[req.params.shortURL].longURL = req.body.longURL;
+  res.redirect('/urls');
+});
+*/
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT} !`);
