ece82ed3bfd9e2d798ac5e7167259fd85386e8d3
NupurPrakash <prakashnupurprakash@gmail.com>
2020-07-07 17:03:36 +0000

    Deleted url and redirect to the urls page

--- express_server.js
+++ express_server.js
@@ -68,12 +68,19 @@ app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
-/*
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls/');
+});
+
+
 app.post("urls/:shortURL", (req, res) => {
   urlDatabase[req.params.shortURL].longURL = req.body.longURL;
   res.redirect('/urls');
 });
-*/
+
+
 
 
 
