055983ea639e087521b2c96b29f76b81d85fc8a7
NupurPrakash <prakashnupurprakash@gmail.com>
2020-07-07 01:57:51 +0000

    Fixed longurl

--- express_server.js
+++ express_server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const PORT = 8080;
 const app = express();
+const bodyParser = require("body-parser")
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.set("view engine", "ejs");
 
@@ -15,7 +19,6 @@ app.get("/", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.send(urlDatabase);
-
 });
 
 app.get("/hello", (req, res) => {
@@ -36,11 +39,19 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: Object.values[req.params.shortURL]};
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT} !`);
